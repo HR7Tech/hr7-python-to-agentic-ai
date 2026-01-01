2c92075918f66a85e82948f98998ce819e8491e2
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-01-02 00:50:54 +0500

    completed day_01 project

---
+++ 100 Days Of Python/Day_01_Beginner_Variables/my_project_studend_id_card.py
@@ -0,0 +1,10 @@
+# My Project: Student ID Card Generator
+
+# This project creates a student ID card using the concepts learned on Day 1.
+
+first_name = input("Enter first name:\n")
+last_name = input("Enter last name:\n")
+course = input("Enter course:\n")
+
+print("Student ID Card:")
+print("First Name = " + first_name + "\nLast Name = " + last_name + "\nCourse = " + course)
---
+++ 100 Days Of Python/Day_01_Beginner_Variables/project.py
@@ -0,0 +1,8 @@
+# Project 1: Band Name Generator
+
+print("Welcome to Band Name Generator...!!!")
+
+user_city = input("Enter the name of the city you grew up in:\n")
+user_pet = input("Enter the name of your pet:\n")
+
+print("Band Name: " + user_city + " " + user_pet)
