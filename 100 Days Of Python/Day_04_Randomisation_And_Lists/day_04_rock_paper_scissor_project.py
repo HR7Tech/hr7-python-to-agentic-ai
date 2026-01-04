65057071e139f5dbfcccc91eec624992e63e9111
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-01-05 04:29:34 +0500

    Updated dAY 04 Rock,Paper,Scissor Project

--- 100 Days Of Python/Day_04_Randomisation_And_Lists/day_04_rock_paper_scissor_project.py
+++ 100 Days Of Python/Day_04_Randomisation_And_Lists/day_04_rock_paper_scissor_project.py
@@ -3,7 +3,11 @@ from day_04_designs_rock_paper_scissor_project import rock,paper,scissors
 
 print("Welcome to Rock , Paper , Scissors.!")
 
-choices = [rock,paper,scissors]
+choices = [rock,paper,scissors] # Imported from day_04 desings file..
+
+# -------------------------------------------------
+
+# User Input
 
 user = int(input("Press '0' for rock , '1' for paper or '2' for scissors: "))
 if user > 2 or user < 0:
@@ -11,12 +15,23 @@ if user > 2 or user < 0:
 else:
     print(f"Player chose:\n{choices[user]}")
 
+# -------------------------------------------------
+
+# Assigning a random value to the computer.
+
 computer = random.randint(0,2)
 print(f"Computer chose:\n{choices[computer]}")
 
-# ROCK 0 BEATS SCISSORS 2
-# PAPER 1 BEATS ROCKS 0
-# SCISSORS 2 BEATS PAPER 1
+# -------------------------------------------------
+
+# Game Rules: 
+# ROCK (0) BEATS SCISSORS (2)
+# PAPER (1) BEATS ROCKS (0)
+# SCISSORS (2) BEATS PAPER (1)
+
+# -------------------------------------------------
+
+# Game Logic:
 
 # DRAW
 if user == computer:
@@ -37,7 +52,7 @@ elif computer == 1 and user == 0:
     print("You Lose.")
 elif computer == 2 and user == 1:
     print("You Lose.")
-    
+
 # INVALID INPUT = NO RESULT
 else:
     print("No Result.")
