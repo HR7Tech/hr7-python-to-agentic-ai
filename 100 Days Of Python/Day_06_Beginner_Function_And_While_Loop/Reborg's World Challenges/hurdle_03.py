8075352fa15c422f2151f0bfa86fe0f71a44d7a0
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-01-16 17:11:42 +0500

    Completed Hurdle 03

---
+++ 100 Days Of Python/Day_06_Beginner_Function_And_While_Loop/Reborg's World Challenges/hurdle_03.py
@@ -0,0 +1,29 @@
+# HURDLE 03:
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+# -------------------------------------------------
+
+# HURDLE 03 SOLUTION:
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
