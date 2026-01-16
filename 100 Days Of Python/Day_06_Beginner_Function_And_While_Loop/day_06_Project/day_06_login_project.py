f788dd306ace3ce75e9b4524890372a4ed4db44b
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-01-17 01:38:11 +0500

    Completed Login Project

--- 100 Days Of Python/Day_06_Beginner_Function_And_While_Loop/day_06_Project/day_06_login_project.py
+++ 100 Days Of Python/Day_06_Beginner_Function_And_While_Loop/day_06_Project/day_06_login_project.py
@@ -1,6 +1,50 @@
-# User Id (Input)
-# User Password (Input)
+user_email = "admin"
+user_password = "admin"
 
-# Function to match user id and password on their second attempt
+def login_successful():
+    print("\nYou are logged in.!")
 
-# while loop until the user id and password are matched..
+def change_email():
+    new_email = input("Enter your new email: ")
+    return new_email
+
+def change_password():
+    new_password = input("Enter your new password: ")
+    return new_password
+
+is_active = True
+
+while is_active:
+    user_id = input("Enter your email: ")
+    user_pass = input("Enter your password: ")
+
+    if user_id == user_email and user_pass == user_password:
+        login_successful()
+        user_choice = input("""Do you want to change your email ID: Type 'I'
+Do you want to change your password: Type 'P'       
+Press 'e' to exit:                    
+""").lower()    
+    
+        if user_choice == "e":
+            is_active = False
+            print("\nGood Bye.!")
+
+        elif user_choice == "i":
+            user_email = change_email()
+            print("\nEmail Change.!")
+            print("Please login again with your new email.! Thank You\n")
+        
+        elif user_choice == "p":
+            user_password = change_password()
+            print("\nPassword Change.!")
+            print("Please login again with your new password.! Thank You\n")
+
+        else:
+            print("\nYou are logged out.!") 
+            print("Sign In again.\n")   
+
+    else:
+        print("\nWrong Email or Password.! Try Again.!\n")        
+
+
+
