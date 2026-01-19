66511ff7cbd9f4ad44396444486a938cd47f5252
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-01-20 01:37:21 +0500

    Completed Decrypt Part

--- 100 Days Of Python/Day_08_Beginner_Functions_Parameters_Arguments/Day_08_Projects/day_08_caesar_cipher_project.py
+++ 100 Days Of Python/Day_08_Beginner_Functions_Parameters_Arguments/Day_08_Projects/day_08_caesar_cipher_project.py
@@ -5,6 +5,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+# -------------------------------------------------
 
 # Encryption
 
@@ -12,6 +13,7 @@ shift = int(input("Type the shift number:\n"))
 
 def encrypt(original_text , shift_amount):
     cipher_text = ""
+
     # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' 
     # forwards in the alphabet by the shift amount and print the encrypted text.
 
@@ -26,9 +28,32 @@ def encrypt(original_text , shift_amount):
 
     print(cipher_text)
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. 
+# TODO-4: Call the 'encrypt()' function and pass in the user inputs. 
 # You should be able to test the code and encrypt a message.
 
 encrypt(original_text = text,shift_amount = shift)
 
+# -------------------------------------------------
+
+# Decryption
+
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* 
+# in the alphabet by the shift amount and print the decrypted text.
+
+def decrypt(original_text , shift_amount):
+    decrypt_text = ""
+
+    for char in original_text:
+        shifted_amount = alphabet.index(char) + shift_amount
+        
+        # TODO-3: What happens if you try to shift z forwards by 9? Can you fix the code?
+
+        shifted_amount %= len(alphabet) # It will keep the shift range from 0 to 25
+
+        decrypt_text += alphabet[shifted_amount]
+
+    print(decrypt_text)
+
+decrypt(original_text = text , shift_amount = shift)
 
