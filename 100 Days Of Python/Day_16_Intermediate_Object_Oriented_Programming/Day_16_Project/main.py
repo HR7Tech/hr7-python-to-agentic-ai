07e2d1ac2868fd5b32cff170aea62623efd2c57c
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-02-12 03:20:03 +0500

    Completed Day 16 Project

---
+++ 100 Days Of Python/Day_16_Intermediate_Object_Oriented_Programming/Day_16_Project/main.py
@@ -0,0 +1,35 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# Program Requirments:
+# 1) Print Report
+# 2) Check Resources Sufficient ?
+# 3) Process Coins
+# 4) Check Transaction Successful ?
+# 5) Make Coffee
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+
+    if choice == "off":
+        is_on = False
+
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+    else:
+        drink = menu.find_drink(choice)
+
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
