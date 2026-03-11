516f35ea9d0efbc6cdba40587e9a46e956a86c93
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-03-12 01:18:01 +0500

    Created paddle class to create multiple paddles in game and implented in main.py

--- 100 Days Of Python/Day_22_Intermediate_Pong_Game/main.py
+++ 100 Days Of Python/Day_22_Intermediate_Pong_Game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 
@@ -6,5 +7,24 @@ screen = Screen()
 screen.setup(width=800 , height=600)
 screen.bgcolor("black")
 screen.title("The Pong Game")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+screen.listen()
+
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+game_on = True
+
+while game_on:
+    screen.update()
+
+
+
 
 screen.exitonclick()
