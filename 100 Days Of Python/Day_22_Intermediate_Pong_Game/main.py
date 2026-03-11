9bf4a4113593703e35c7200616468a123f553dff
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-03-12 00:18:49 +0500

    Screen Setup Completed

---
+++ 100 Days Of Python/Day_22_Intermediate_Pong_Game/main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+screen = Screen()
+
+# Setting Up Screen
+screen.setup(width=800 , height=600)
+screen.bgcolor("black")
+screen.title("The Pong Game")
+
+screen.exitonclick()
