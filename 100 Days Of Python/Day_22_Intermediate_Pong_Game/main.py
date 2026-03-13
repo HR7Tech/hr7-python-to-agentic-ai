82f5499aea4603b35b3807d2231a9a4b652113ce
Hamid Raza <hamidrazahr7.13@gmail.com>
2026-03-13 23:19:13 +0500

    Added a function if a certain paddle misses the ball , the ball restarts from the middle and move to the opposite paddle

--- 100 Days Of Python/Day_22_Intermediate_Pong_Game/main.py
+++ 100 Days Of Python/Day_22_Intermediate_Pong_Game/main.py
@@ -33,8 +33,23 @@ while game_on:
     # Detect collision with walls
     if ball.ycor() > 280 or ball.ycor() < -280:
         # Ball Bounces
-        ball.bounce()
+        ball.bounce_y()
 
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320 :
+        # Ball Bounces
+        ball.bounce_x()
+
+    # If a right paddle misses the ball, resart from the middle and move to opposite paddle.
+    if ball.xcor() > 400:
+            ball.restart() 
+
+    # If a left paddle misses the ball, resart from the middle and move to opposite paddle.  
+    if ball.xcor() < -400:
+            ball.restart()       
+
+    
+            
     
     
 
